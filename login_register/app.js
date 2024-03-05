8f5a26f92125aefdfa00f7147934a8ce88a691aa
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-05 16:17:05 +0800

    ENH: Add toggle visibility

--- login_register/app.js
+++ login_register/app.js
@@ -9,3 +9,19 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+
+const passwordField = document.querySelector("input[type='password']");
+const togglePassword = document.querySelector(".password-toggle-icon i");
+
+togglePassword.addEventListener("click", function () {
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    togglePassword.classList.remove("fa-eye");
+    togglePassword.classList.add("fa-eye-slash");
+  } else {
+    passwordField.type = "password";
+    togglePassword.classList.remove("fa-eye-slash");
+    togglePassword.classList.add("fa-eye");
+  }
+});
