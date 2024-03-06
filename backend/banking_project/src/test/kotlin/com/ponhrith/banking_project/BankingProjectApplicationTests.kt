6140b4b59e38a8fb319dc4517082fb4a3e3f4080
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-06 16:09:32 +0800

    Initial Commit

---
+++ backend/banking_project/src/test/kotlin/com/ponhrith/banking_project/BankingProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ponhrith.banking_project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankingProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
