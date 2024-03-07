7aea979d2adf6ae4e29e60251d1f7339d3e15bb1
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-07 14:16:05 +0800

    TEST:user profile seeding

---
+++ backend/banking_project/src/main/kotlin/com/ponhrith/banking_project/common/ProfileSeeding.kt
@@ -0,0 +1,14 @@
+package com.ponhrith.banking_project.common
+//
+//import com.ponhrith.banking_project.repository.ProfileRepository
+//import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.boot.CommandLineRunner
+//import org.springframework.stereotype.Component
+//
+//@Component
+//class ProfileSeeding : CommandLineRunner{
+//    @Autowired
+//    private lateinit var profileRepository: ProfileRepository
+//
+//    @Autowired
+//}
