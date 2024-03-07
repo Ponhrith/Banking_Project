3710364cc120621e95f10b34daed0f087de04fca
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-07 12:11:52 +0800

    TST:profile repository

---
+++ backend/banking_project/src/main/kotlin/com/ponhrith/banking_project/repository/ProfileRepository.kt
@@ -0,0 +1,11 @@
+package com.ponhrith.banking_project.repository
+
+import com.ponhrith.banking_project.model.Profile
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface ProfileRepository : JpaRepository<Profile, Long> {
+    override fun findById(id: Long): Optional<Profile>
+}
