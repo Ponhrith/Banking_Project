a8241714223116786eee0790856e97bd0866d4c5
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-08 14:26:02 +0800

    TEST:jwt request filter

--- backend/banking_project/src/main/kotlin/com/ponhrith/banking_project/config/JwtRequestFilter.kt
+++ backend/banking_project/src/main/kotlin/com/ponhrith/banking_project/config/JwtRequestFilter.kt
@@ -1,36 +1,30 @@
 package com.ponhrith.banking_project.config
 
-
 import com.ponhrith.banking_project.service.AuthService
-import jakarta.servlet.FilterChain
-import jakarta.servlet.ServletException
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.util.StringUtils
-import org.springframework.web.filter.GenericFilterBean
-import org.springframework.web.filter.OncePerRequestFilter
-import java.io.IOException
-import java.util.logging.Filter
+import javax.servlet.Filter
+import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
-
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Component
-class JwtRequestFilter : GenericFilterBean(), Filter {
+class JwtRequestFilter : Filter {
     @Autowired
     private lateinit var jwtUtil: JwtUtil
 
     @Autowired
     private lateinit var userDetailsService: AuthService
 
-    @Throws(IOException::class, ServletException::class)
     override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
         val httpRequest = request as HttpServletRequest
+        val httpResponse = response as HttpServletResponse
         val authorizationHeader = httpRequest.getHeader("Authorization")
         val jwtToken: String?
         val username: String?
@@ -49,5 +43,9 @@ class JwtRequestFilter : GenericFilterBean(), Filter {
             }
         }
         chain.doFilter(request, response)
+    }
+
+    override fun destroy() {
+        super.destroy()
     }
 }
