c27401131fc27557d8666729d7b5bcd1b0179f7b
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-11 18:25:33 +0800

    FEAT:login controller

---
+++ backend/banking_project/src/main/kotlin/com/ponhrith/banking_project/controller/LoginController.kt
@@ -0,0 +1,18 @@
+package com.ponhrith.banking_project.controller
+
+import com.ponhrith.banking_project.controller.request.LoginReq
+import com.ponhrith.banking_project.controller.response.LoginRes
+import com.ponhrith.banking_project.service.LoginService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@CrossOrigin("http://localhost:8080/")
+class LoginController(
+    private val loginService: LoginService
+) {
+    @PostMapping("/login")
+    fun login(@RequestBody loginReq: LoginReq): LoginRes {
+        return loginService.login(loginReq)
+    }
+}
