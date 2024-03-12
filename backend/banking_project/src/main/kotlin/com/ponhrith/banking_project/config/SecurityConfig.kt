a40396f19135d3b75182553943c2f071ac954f05
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-12 14:40:59 +0800

    FEAT:cors filter

--- backend/banking_project/src/main/kotlin/com/ponhrith/banking_project/config/SecurityConfig.kt
+++ backend/banking_project/src/main/kotlin/com/ponhrith/banking_project/config/SecurityConfig.kt
@@ -2,14 +2,42 @@ package com.ponhrith.banking_project.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
 
 @Configuration
-class SecurityConfig {
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
     }
+
+    override fun configure(http: HttpSecurity) {
+        http
+            .cors().and()
+            .csrf().disable()
+            .authorizeRequests()
+            .anyRequest().authenticated().and()
+            .formLogin().and()
+            .httpBasic()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration().applyPermitDefaultValues()
+        configuration.allowedOrigins = listOf("http://127.0.0.1:5500")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+
 }
