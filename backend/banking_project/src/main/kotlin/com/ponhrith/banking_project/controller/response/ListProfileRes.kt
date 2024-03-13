50669cfef87eacff647ba1dc5ae566a1ce9be672
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-13 17:08:13 +0800

    ENH:list profile res

---
+++ backend/banking_project/src/main/kotlin/com/ponhrith/banking_project/controller/response/ListProfileRes.kt
@@ -0,0 +1,9 @@
+package com.ponhrith.banking_project.controller.response
+
+data class ListProfileRes(
+    val id: Long,
+    val fullname: String,
+    val address: String,
+    val email: String,
+    val accounts: List<AccountRes>
+)
