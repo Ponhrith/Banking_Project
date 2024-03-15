04ee83df581218b5fa6a49b7e93a4d01ff294339
Ponhrith <ponhrith.nhep@gmail.com>
2024-03-15 10:47:33 +0800

    ENH:transcation req

---
+++ backend/banking_project/src/main/kotlin/com/ponhrith/banking_project/controller/request/TransactionReq.kt
@@ -0,0 +1,12 @@
+package com.ponhrith.banking_project.controller.request
+
+import java.time.LocalDate
+
+data class TransactionReq(
+    val profiileId: Long,
+    var sourceAccount: Long,
+    val type: String,
+    var targetAccount: Long,
+    var amount: Double,
+    var date: LocalDate
+)
---
+++ backend/banking_project/src/main/kotlin/com/ponhrith/banking_project/controller/response/TransactionRes.kt
@@ -0,0 +1,3 @@
+package com.ponhrith.banking_project.controller.response
+
+data class TransactionRes()
